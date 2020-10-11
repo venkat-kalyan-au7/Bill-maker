d4807912f319792a09b898d3b33140929c3588ab
Venkat Kalyan <60262281+venkat-kalyan-au7@users.noreply.github.com>
2020-10-11 17:28:38 +0530

    Update Card.js

--- client/src/Pages/ProductCard/Card.js
+++ client/src/Pages/ProductCard/Card.js
@@ -18,7 +18,7 @@ const Card = ({
       <div className="card-header card-header-2 ">{product.productName}</div>
       <div className="card-body">
         
-        <img src={product.images[0]} style={{width: '150px', height: '150px'}} />
+        <img src={product.images[0]} style={{width: '250px', height: '250px'}} />
       
         <p className="card-p black-10">₹ {product.price}</p>
       
